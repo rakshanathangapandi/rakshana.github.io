f088715b89aa22a310473a53c89be1c100294cb5
aceraksh <rakshan0717@gmail.com>
2026-08-02 23:10:47 +0530

    profile updation

--- pelicanconf.py
+++ pelicanconf.py
@@ -24,7 +24,7 @@ PYGMENTS_STYLE_DARK = 'native'  # Dark mode
 
 # Static files
 STATIC_PATHS = ['images']
-SITELOGO = '/images/rakshana.png'
+SITELOGO = 'images/rakshana.png'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
